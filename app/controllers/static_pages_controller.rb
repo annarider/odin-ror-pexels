8c5993b03f2001f9fc85f9f38f0e075fc0bdd3dd
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-29 10:46:36 -0600

    feat: add StaticPagesController#home action logic

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
+    if params[:collection_id].present?
+      client = Pexels::Client.new(ENV['PEXELS_API_KEY'])
+      @photos = client.collection_media(params[:collection_id])
+    end
   end
 end
