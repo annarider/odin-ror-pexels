23267f7f5e5d562ebf7903d642b5f9fdd552c379
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-29 10:46:36 -0600

    fix: collections (correct method) per API docs

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if params[:collection_id].present?
       client = Pexels::Client.new(ENV['PEXELS_API_KEY'])
-      @photos = client.collection_media(params[:collection_id])
+      @photos = client.collections[params[:collection_id]].media
     end
   end
 end
